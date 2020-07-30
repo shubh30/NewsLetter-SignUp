b6cca233527323d80d0f8ad53e38f6f71668c996
Shubham <shubhamnagpal30@gmail.com>
2020-07-30 12:20:08 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/', function(req, res) {
+  console.log(req.body.fname);
+  console.log(req.body.lname);
+  console.log(req.body.email);
+})
+
+app.listen(3000, function () {
+  console.log('Server is running on PORT: 3000');
+})
